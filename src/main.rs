5143bf0233bd38429c8b11d3eb4d9513aff2a58c
Yohei Tamura <tamuhey@gmail.com>
2020-08-24 14:01:48 +0900

    hold string directly in Vocab

--- src/main.rs
+++ src/main.rs
@@ -4,51 +4,28 @@ use std::{
     io::{self, Read},
 };
 
-#[derive(Clone, Debug, Eq, PartialEq, Hash, Copy)]
-enum Data {
-    Prime(char),
-    Pair((u32, u32)),
-}
-use Data::*;
-
 enum EncodeOpt {
     NTimes(u32),
 }
 
 #[derive(Debug, Default, Clone)]
 struct Vocab {
-    table: Vec<Data>,
-    inv: HashMap<Data, u32>,
+    table: Vec<String>,
+    inv: HashMap<String, u32>,
 }
 
 impl Vocab {
     pub fn len(&self) -> usize {
         self.table.len()
     }
-    pub fn push(&mut self, c: Data) -> u32 {
+    pub fn push(&mut self, c: String) -> u32 {
         if !self.inv.contains_key(&c) {
             let i = self.len() as u32;
+            self.table.push(c.clone());
             self.inv.insert(c, i);
-            self.table.push(c);
             i
         } else {
-            self.inv[&c]
-        }
-    }
-
-    pub fn decode(&self, data: &[u32], s: &mut String) {
-        for &x in data {
-            self.decode_unit(x, s)
-        }
-    }
-
-    fn decode_unit(&self, x: u32, s: &mut String) {
-        match self.table[x as usize] {
-            Prime(c) => s.push(c),
-            Pair((y, z)) => {
-                self.decode_unit(y, s);
-                self.decode_unit(z, s);
-            }
+            self.inv[c.as_str()]
         }
     }
 
@@ -69,31 +46,35 @@ impl Vocab {
 
     /// Encode `dat` for one step
     /// Returns whether the step proceeded or not.
-    fn encode_step<'a>(
+    fn encode_step(
         &mut self,
-        dat: &'a mut Vec<u32>,
-        new_dat: &'a mut Vec<u32>,
-        counter: &mut HashMap<Data, u32>,
+        dat: &mut Vec<u32>,
+        new_dat: &mut Vec<u32>,
+        counter: &mut HashMap<(u32, u32), u32>,
     ) -> bool {
         new_dat.clear();
         counter.clear();
 
         // Find most frequently occuring pair
         for (&s, &t) in dat.iter().zip(dat.iter().skip(1)) {
-            let c = Pair((s, t));
+            let c = (s, t);
             *counter.entry(c).or_insert(0) += 1;
         }
-        let best: Data = if let Some(v) = counter.iter().max_by_key(|(_, v)| **v).map(|(k, _)| k) {
-            *v
-        } else {
-            return false;
-        };
+        let best: (u32, u32) =
+            if let Some(v) = counter.iter().max_by_key(|(_, v)| **v).map(|(k, _)| k) {
+                *v
+            } else {
+                return false;
+            };
 
         // Encode the pair
-        let code = self.push(best);
+        let mut s = String::new();
+        self.decode_unit(best.0, &mut s);
+        self.decode_unit(best.1, &mut s);
+        let code = self.push(s);
         let mut i = 0;
         while i < dat.len() {
-            if i != dat.len() - 1 && Pair((dat[i], dat[i + 1])) == best {
+            if i != dat.len() - 1 && (dat[i], dat[i + 1]) == best {
                 new_dat.push(code);
                 i += 1;
             } else {
@@ -110,10 +91,19 @@ impl Vocab {
     fn encode_first_step(&mut self, text: &str) -> Vec<u32> {
         let mut dat: Vec<u32> = vec![];
         for c in text.chars() {
-            let c = Prime(c);
-            dat.push(self.push(c));
+            dat.push(self.push(c.to_string()));
         }
         dat
+    }
+
+    pub fn decode(&self, data: &[u32], s: &mut String) {
+        for &x in data {
+            self.decode_unit(x, s)
+        }
+    }
+
+    fn decode_unit(&self, x: u32, s: &mut String) {
+        s.push_str(self.table[x as usize].as_str())
     }
 }
 
